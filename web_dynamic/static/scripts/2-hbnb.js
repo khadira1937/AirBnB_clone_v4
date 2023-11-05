67c17071def4a0ad9aefe0ab0e183d17c40e92d3
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-11-05 07:09:24 -0800

    add task04

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -6,21 +6,16 @@ $( document ).ready(function() {
         } else {
             delete amenities[$(this).attr('data-id')];
         }
-	console.log(amenities);
         if (Object.keys(amenities).length >= 0) {
             $("DIV.amenities H4").text(Object.values(amenities).join(', '));
         }
     });
-    const link = "http://" + window.location.hostname;
-  $(function () {
-    const apiUrl = link + ":5001/api/v1/status/";
-    $.get(apiUrl, function (data, status) {
-      if (data.status === "OK" && status === "success") {
-        $("#api_status").addClass("available");
-      } else {
-        $("#api_status").removeClass("available");
-      }
-    });
-  });
-});
 
+$.get('http://0.0.0.0:5001/api/v1/status/', function(data)
+{
+    if (data.status == "OK"){
+	$('#api_status').addClass('available');
+    } else {
+	$('#api_status').removeClass('available');
+    }
+});
